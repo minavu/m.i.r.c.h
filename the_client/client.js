9709623ff8499cb89bd2b578b19fc1c59f58fd10
Mina <minavu@Minas-MacBook-Pro.local>
2022-03-03 16:41:46 -0800

    Working on m.i.r.c.h

--- the_client/client.js
+++ the_client/client.js
@@ -1,15 +1,67 @@
 const socket = io("http://localhost:5001");
 
 socket.on("welcome", (data) => {
-  let { welcome, socket } = data;
-  socket = socket.substr(0, 4).toUpperCase();
-  console.log(
-    `${welcome}, ${socket}! We'll use the shorthand ${socket} for you.`
-  );
+  let { welcome, socket, rooms } = data;
+  console.log(`${welcome}, ${socket}! Check out all rooms: ${rooms}`);
+  displayAllRooms(rooms, "");
 });
 
 socket.on("message", (data) => {
+  let { socket, message } = data;
   console.log("Server relayed: ", data);
+  let chat_screen = document.getElementById("chat-screen");
+
+  let p = document.createElement("p");
+  p.innerHTML = socket + ":  " + message;
+
+  chat_screen.append(p);
+  p.scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+});
+
+socket.on("announcement", (data) => {
+  let { socket, announce, room } = data;
+  let chat_screen = document.getElementById("chat-screen");
+
+  let p = document.createElement("p");
+  p.innerHTML = `${socket} ${announce} ${room}`;
+
+  chat_screen.append(p);
+  p.scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+});
+
+socket.on("joined_room", (data) => {
+  console.log("Server created room per request: ", data);
+  let { current, rooms } = data;
+  displayAllRooms(rooms, current);
+
+  let title = document.getElementById("title");
+  title.innerHTML = `Room ${current}`;
+
+  let btn = document.createElement("input");
+  btn.setAttribute("type", "button");
+  btn.setAttribute("onclick", "btnHandler(event)");
+  btn.setAttribute("value", "Leave");
+  btn.setAttribute("name", current);
+  btn.classList.add("me-2", "btn", "btn-outline-dark");
+  btn.style.display = "inline-block";
+  title.insertBefore(btn, title.firstChild);
+
+  document.getElementById("chatbox").disabled = false;
+  document.getElementById("submit-btn").disabled = false;
+});
+
+socket.on("left_room", (data) => {
+  console.log("Server announced: ", data);
+  let { current, rooms } = data;
+
+  displayAllRooms(rooms, current);
+
+  let title = document.getElementById("title");
+  title.removeChild(title.firstChild);
+  title.innerHTML = "Select a room...";
+
+  document.getElementById("chatbox").disabled = true;
+  document.getElementById("submit-btn").disabled = true;
 });
 
 document.querySelector("form").addEventListener("submit", (event) => {
@@ -18,3 +70,62 @@ document.querySelector("form").addEventListener("submit", (event) => {
   socket.emit("message", message.value);
   message.value = "";
 });
+
+const btnHandler = (event) => {
+  // console.log(event.target.value);
+  // console.log(event.target.name);
+
+  if (event.target.value === "Create") {
+    socket.emit("create_room");
+  }
+
+  if (event.target.value === "Leave") {
+    socket.emit("leave_room");
+  }
+};
+
+const displayAllRooms = (rooms, current) => {
+  document.querySelectorAll(".room-line").forEach((line) => line.remove());
+  let display_rooms = document.getElementById("rooms");
+  rooms.forEach((room) => {
+    let buttonBootstrapColor = "btn-outline-primary";
+    let buttonMessage = "Join";
+    if (room === current) {
+      buttonBootstrapColor = "btn-outline-success";
+      buttonMessage = "In";
+    }
+    appendRoomDescription(
+      display_rooms,
+      buttonBootstrapColor,
+      buttonMessage,
+      room
+    );
+  });
+  appendRoomDescription(display_rooms, "btn-danger", "Create", "New Room");
+};
+
+const appendRoomDescription = (
+  parentElement,
+  buttonBootstrapColor,
+  buttonMessage,
+  roomName
+) => {
+  let div = document.createElement("div");
+  div.setAttribute("class", "room-line");
+
+  let btn = document.createElement("input");
+  btn.setAttribute("type", "button");
+  btn.setAttribute("onclick", "btnHandler(event)");
+  btn.setAttribute("value", buttonMessage);
+  btn.setAttribute("name", roomName);
+  btn.classList.add("me-2", "btn", buttonBootstrapColor);
+  btn.style.display = "inline-block";
+  div.append(btn);
+
+  let p = document.createElement("p");
+  p.style.display = "inline-block";
+  p.textContent = roomName;
+  div.append(p);
+
+  parentElement.append(div);
+};
--- the_server/server.js
+++ the_server/server.js
@@ -6,31 +6,78 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const port = 5001;
+const { uniqueNamesGenerator, colors } = require("unique-names-generator");
 
 app.use(express.static(path.join(__dirname, "../the_client")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../the_client/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 io.on("connection", (socket) => {
-  console.log(`client with socket id ${socket.id} connected`);
+  socket.leave(socket.id);
+  let sock_id = socket.id.substring(0, 4).toUpperCase();
+  let curr_room = "";
+  console.log(`client with socket id ${sock_id} connected`);
+
+  // socket.join("lobby");
 
   socket.emit("welcome", {
     welcome: "Welcome to the chat",
-    socket: socket.id,
+    socket: sock_id,
+    rooms: getAllRooms(),
   });
 
   socket.on("message", (data) => {
-    console.log("Client says: ", data);
+    // console.log("Client says: ", data);
 
-    io.emit("message", {
-      socket: socket.id,
+    io.to(curr_room).emit("message", {
+      socket: sock_id,
       message: data,
     });
   });
+
+  socket.on("create_room", () => {
+    let new_room = uniqueNamesGenerator({ dictionaries: [colors] });
+    socket.leave(curr_room);
+    socket.join(new_room);
+    curr_room = new_room;
+
+    socket.emit("joined_room", {
+      socket: sock_id,
+      status: "created and join new room",
+      current: curr_room,
+      rooms: getAllRooms(),
+    });
+  });
+
+  socket.on("leave_room", () => {
+    io.to(curr_room).emit("announcement", {
+      socket: sock_id,
+      announce: "has left room",
+      room: curr_room,
+    });
+
+    socket.leave(curr_room);
+    curr_room = "";
+
+    socket.emit("left_room", {
+      socket: sock_id,
+      status: "left room",
+      current: curr_room,
+      rooms: getAllRooms(),
+    });
+  });
+
+  function getAllRooms() {
+    let all_rooms_list = [];
+    io.of("/").adapter.rooms.forEach((value, key) => {
+      all_rooms_list.push(key);
+    });
+    return all_rooms_list;
+  }
 });
 
 server.listen(port, () => {
-  console.log(`Application running at "http://localhost:${port}/"`);
+  console.log(`M.I.R.C.H Application running at "http://localhost:${port}/"`);
 });
